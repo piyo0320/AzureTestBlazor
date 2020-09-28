18b7e444a2fa601509fcdcb1cabed821982715a1
piyo8810 <piyo8810.8@gmail.com>
2020-09-29 00:01:18 +0900

    first commit

---
+++ AzureTestBlazor/Shared/ViewText.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureTestBlazor.Shared
+{
+    public class ViewText
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
